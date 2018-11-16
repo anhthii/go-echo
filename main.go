5494301f1e0d35aad4df8b5ee00bce649ad8e5d3
anhthi <nguyendanganhthi247@gmail.com>
2018-11-16 07:59:37 +0700

    seperate router from main

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/anhthii/go-echo/db"
 	"github.com/anhthii/go-echo/db/models"
-
-	"github.com/anhthii/go-echo/handlers/media"
-	playlistHandlers "github.com/anhthii/go-echo/handlers/playlist"
-	userHandlers "github.com/anhthii/go-echo/handlers/user"
-	authMiddlewares "github.com/anhthii/go-echo/middlewares"
-	"github.com/gin-gonic/gin"
+	"github.com/anhthii/go-echo/router"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -18,57 +17,14 @@ func main() {
 	db.Tables(&models.User{}, &models.Playlist{}, &models.Song{}, &models.Artist{})
 	defer db.Close()
 
-	router := SetupRouter()
-	router.Run(":3000")
-}
-
-// SetupRouter registers routes
-func SetupRouter() *gin.Engine {
-	router := gin.Default()
-	api := router.Group("/api")
-	{
-		mediaRoutes := api.Group("/media")
-		{
-			mediaRoutes.GET("/song", media.GetSong)
-			mediaRoutes.GET("/albums/default", media.GetDefaultAlbums)
-			mediaRoutes.GET("/albums", media.GetAlbums)
-			mediaRoutes.GET("/artists/default", media.GetDefaultArtists)
-			mediaRoutes.GET("/artists", media.GetArtists)
-			mediaRoutes.GET("/artist/:name/:type", media.GetSingleArtist)
-			mediaRoutes.GET("/chart/:popType", media.GetChart)
-			mediaRoutes.GET("/suggested-song", media.GetSuggestedSongs)
-			mediaRoutes.GET("/album_playlist", media.GetAlbumPlaylist)
-			mediaRoutes.GET("/top100/:typeID", media.GetTop100)
-			mediaRoutes.GET("/search", media.SearchSong)
-		}
-
-		userRoutes := api.Group("/user")
-		{
-			userRoutes.POST("/signup", userHandlers.CreateNewUser)
-			userRoutes.POST("/login", userHandlers.Login)
-
-		}
-
-		playlistRoutes := api.Group("/playlist", authMiddlewares.TokenAuthMiddleware())
-		{
-			// get all playlists of a user
-			playlistRoutes.GET("/:username", authMiddlewares.IsValidUser, playlistHandlers.GetPlaylists)
-
-			// get a specific playlist with title
-			// not implemented yet
-			// playlistRoutes.GET("/:username/:title", getPlaylist)
-			playlistRoutes.POST("/:username", authMiddlewares.IsValidUser, playlistHandlers.CreatePlaylist)
-
-			// // delete a playlist
-			playlistRoutes.DELETE("/:username/:playlistTitle", authMiddlewares.IsValidUser, playlistHandlers.DeletePlaylist)
-
-			// add a song to a playlist
-			playlistRoutes.PUT("/:username/:playlistTitle", authMiddlewares.IsValidUser, playlistHandlers.AddSongToPlaylist)
-
-			// delete a song from a playlist
-			// playlistRoutes.DELETE("/:username/:playlistTitle/:songId", deleteSongFromPlaylist)
-		}
+	err := godotenv.Load()
+	if err != nil {
+		log.Panic("Error loading .env file")
 	}
-	// router.GET("/download/song/:songTitle/:id", handlers.DownloadSong)
-	return router
+
+	MODE := os.Getenv("SERVER_MODE")
+	router := router.SetupRouter(MODE)
+
+	PORT := os.Getenv("SERVER_PORT")
+	router.Run(":" + PORT)
 }
---
+++ router/router.go
@@ -0,0 +1,64 @@
+package router
+
+import (
+	"github.com/anhthii/go-echo/handlers/media"
+	playlistHandlers "github.com/anhthii/go-echo/handlers/playlist"
+	userHandlers "github.com/anhthii/go-echo/handlers/user"
+	authMiddlewares "github.com/anhthii/go-echo/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(mode string) *gin.Engine {
+	router := gin.Default()
+
+	if mode == "prod" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	api := router.Group("/api")
+	{
+		mediaRoutes := api.Group("/media")
+		{
+			mediaRoutes.GET("/song", media.GetSong)
+			mediaRoutes.GET("/albums/default", media.GetDefaultAlbums)
+			mediaRoutes.GET("/albums", media.GetAlbums)
+			mediaRoutes.GET("/artists/default", media.GetDefaultArtists)
+			mediaRoutes.GET("/artists", media.GetArtists)
+			mediaRoutes.GET("/artist/:name/:type", media.GetSingleArtist)
+			mediaRoutes.GET("/chart/:popType", media.GetChart)
+			mediaRoutes.GET("/suggested-song", media.GetSuggestedSongs)
+			mediaRoutes.GET("/album_playlist", media.GetAlbumPlaylist)
+			mediaRoutes.GET("/top100/:typeID", media.GetTop100)
+			mediaRoutes.GET("/search", media.SearchSong)
+		}
+
+		userRoutes := api.Group("/user")
+		{
+			userRoutes.POST("/signup", userHandlers.CreateNewUser)
+			userRoutes.POST("/login", userHandlers.Login)
+
+		}
+
+		playlistRoutes := api.Group("/playlist", authMiddlewares.TokenAuthMiddleware())
+		{
+			// get all playlists of a user
+			playlistRoutes.GET("/:username", authMiddlewares.IsValidUser, playlistHandlers.GetPlaylists)
+
+			// get a specific playlist with title
+			// not implemented yet
+			// playlistRoutes.GET("/:username/:title", getPlaylist)
+			playlistRoutes.POST("/:username", authMiddlewares.IsValidUser, playlistHandlers.CreatePlaylist)
+
+			// // delete a playlist
+			playlistRoutes.DELETE("/:username/:playlistTitle", authMiddlewares.IsValidUser, playlistHandlers.DeletePlaylist)
+
+			// add a song to a playlist
+			playlistRoutes.PUT("/:username/:playlistTitle", authMiddlewares.IsValidUser, playlistHandlers.AddSongToPlaylist)
+
+			// delete a song from a playlist
+			// playlistRoutes.DELETE("/:username/:playlistTitle/:songId", deleteSongFromPlaylist)
+		}
+	}
+	// router.GET("/download/song/:songTitle/:id", handlers.DownloadSong)
+	return router
+}
